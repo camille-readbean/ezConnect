ca38e0974ecd667490b049c3f8ebde023b5482b8
Chin Rui Bin <52031118+camille-readbean@users.noreply.github.com>
2023-06-19 13:40:04 +0800

    Add data for prepopulating course

--- backend/pre_data/prepare.py
+++ backend/pre_data/prepare.py
@@ -1,6 +1,7 @@
 import csv
 import uuid
 import pathlib
+import json
 
 if not pathlib.Path('degrees.csv').exists():
     print("Creating UUIDs for degrees.csv")
@@ -24,13 +25,29 @@ if not pathlib.Path('programmes.csv').exists():
 else:
     print("skipping creating UUID for programmes_raw.csv")
 
-# if not pathlib.Path('courses.csv').exists():
-#     print("Creating UUIDs for programmes.csv")
-#     with open('programmes_raw.csv', 'r') as input_file, open('programmes.csv', 'w') as output_file:
-#         reader = csv.reader(input_file)
-#         writer = csv.writer(output_file)
-#         writer.writerow(['id', 'title'])
-#         for row in reader:
-#             writer.writerow([str(uuid.uuid4()), row[0]])
-# else:
-#     print("skipping creating UUID for programmes_raw.csv")
+def check_semester(course: dict, sem: int):
+    for semester_data in course['semesterData']:
+        if semester_data['semester'] == sem:
+            return True
+    return False
+
+if not pathlib.Path('courses.csv').exists():
+    print("Creating UUIDs for courses.csv")
+    with open('moduleInfo.json', 'r') as input_file, open('courses.csv', 'w') as output_file:
+        courses = json.load(input_file)
+        writer = csv.writer(output_file)
+        writer.writerow(['course_code', 'course_name', 'number_of_units', \
+                         'is_offered_in_sem1', 'is_offered_in_sem1'])
+        print(f"{len(courses)} courses found")
+        for course in courses:
+            offered_sem_1, offered_sem_2 = check_semester(course, 1), check_semester(course, 2)
+            row = [
+                course['moduleCode'],
+                course['title'],
+                course['moduleCredit'],
+                offered_sem_1,
+                offered_sem_2
+            ]
+            writer.writerow(row)
+else:
+    print("skipping creating UUID for programmes_raw.csv")
